a9b4e7238dec5b367493a3d51948ad6774e068a9
patrykdudzinski <patryk@dudzinski.com.pl>
2016-12-22 22:30:51 +0100

    spakowane zadanie do grupy frontend

--- script1.js
+++
@@ -1,6 +0,0 @@
-var kliki = 0;
-function onClick() {
-    kliki = kliki + 1;
-    document.getElementById("clickCount").innerHTML = kliki;
-}
-
--- script2.js
+++
@@ -1,5 +0,0 @@
- var licznik = setInterval(
-      function (){
-        document.getElementById("clickCount").innerHTML = 0;
-        kliki = 0;
-        clearInterval; } ,30000);          
